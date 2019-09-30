f5da2f8fc7c86c25ceb1aa9cc26a30e73771d314
李宏源 <iris.li@gymboglobal.com>
2019-09-30 17:18:07 +0800

    add advance high oop .py

--- .idea/high.py
+++ .idea/high.py
@@ -45,3 +45,17 @@ person('Adam', 45, gender='M', job='Engineer') #name: Adam age: 45 other: {'gend
 def person(name, age, *, city, job):
     print(name, age, city, job)
 
+# 列表生成式
+[x*x for x in range(1,11)]  #[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+[x*x for x in range(1,11) if x % 2 == 0]    #筛选出仅偶数的平方 [4, 16, 36, 64, 100]
+[m+n for m in 'ABC' for n in 'XYZ']     #可以使用两层循环生成全排列 ['AX', 'AY', 'AZ', 'BX', 'BY', 'BZ', 'CX', 'CY', 'CZ']
+import os
+[d for d in os.listdir('.')]            #列出当前目录下的所有文件和目录名 ['advance.py', 'basic.py', 'high.py', 'misc.xml', 'modules.xml', 'OOP.py', 'test.py', 'vcs.xml', 'workspace.xml']
+d={'x':'A','y':'B','z':'C'}
+[k + '=' + v for k,v in d.items()]      #使用两个变量来生成list   ['x=A', 'y=B', 'z=C']
+L=['Hello','World','IBM','Apple']
+[s.lower() for s in L]                  #把所有字符串都变成小写    ['hello', 'world', 'ibm', 'apple']
+x='abc'
+y=123
+isinstance(x,str)                       #使用内建的isinstance函数判断一个变量是不是字符串    True
+isinstance(y,str)                       #使用内建的isinstance函数判断一个变量是不是字符串    False
