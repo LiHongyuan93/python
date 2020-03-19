1bdb386fdcb8fe201e08cb46f37a904d8e93a25f
iris <iris.li@gymboglobal.com>
2020-03-19 18:02:56 +0800

    add practice script: print triangle

--- .idea/print_triangle.py
+++ .idea/print_triangle.py
@@ -1,5 +1,20 @@
 # coding=UTF-8
+# python version: Python 3.7.6
 # 打印等腰倒三角
+# output：
+# /usr/local/bin/python3.7 /Users/gymboadmin/python/python/.idea/print_triangle.py
+# 请出入行数:10
+#  * * * * * * * * * *
+#   * * * * * * * * *
+#    * * * * * * * *
+#     * * * * * * *
+#      * * * * * *
+#       * * * * *
+#        * * * *
+#         * * *
+#          * *
+#           *
+
 
 row = int(input("请出入行数:"))
 
@@ -11,3 +26,4 @@ for line in range(1,row+1):
     for start in range(1,startNum,1):
         print("*",end=' ')
     print("")
+
